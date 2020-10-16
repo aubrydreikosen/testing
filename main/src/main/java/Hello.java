20a923a311efa38fa99f662a40c0276172ff01e8
alan <azc@live.unc.edu>
2020-10-16 18:12:31 -0400

    test

--- main/src/main/java/Hello.java
+++ main/src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("please add a message below this");
     // testing hii
+    System.out.println("alan");
+
   }
-    System.out.println("alan!");
 }
